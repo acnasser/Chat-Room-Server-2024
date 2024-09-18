27f1b42e3eb20a7922a4481e4c7556ccab021185
acnasser <acn8701@uncw.edu>
2024-09-18 12:01:29 -0400

    Initial commit with chat server and client code

--- chat_server.py
+++ chat_server.py
@@ -1,46 +1,41 @@
 import socket
-import select
-from threading import *
-import sys
-
+import threading
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-"""
-the first argument AF_INET is the address domain of the socket. This is used when we have an Internet Domain
-with any two hosts
-The second argument is the type of socket. SOCK_STREAM means that data or characters are read in a continuous flow
-"""
 server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-if len(sys.argv) != 3:
-    print("Correct usage: script, IP address, port number")
-    exit()
-IP_address = str(sys.argv[1])
-Port = int(sys.argv[2])
-server.bind((IP_address, Port)) 
-#binds the server to an entered IP address and at the specified port number. The client must be aware of these parameters
+
+# Hardcode your IP address and Port here
+IP_address = socket.gethostbyname(socket.gethostname())  # or use '127.0.0.1' for localhost
+Port = 8080  # Use any available port you want
+
+server.bind((IP_address, Port))
 server.listen(100)
-#listens for 100 active connections. This number can be increased as per convenience
-list_of_clients=[]
+
+list_of_clients = []
 
 def clientthread(conn, addr):
-    conn.send("Welcome to this chatroom!")
-    #sends a message to the client whose user object is conn
-    while True:
-            try:     
-                message = conn.recv(2048)    
-                if message:
-                    print("<" + addr[0] + "> " + message)
-                    message_to_send = "<" + addr[0] + "> " + message
-                    broadcast(message_to_send,conn)
-                    #prints the message and address of the user who just sent the message on the server terminal
-                else:
-                    remove(conn)
-            except:
-                continue
+    # Receive client ID from the client as the first message
+    client_id = conn.recv(2048).decode('utf-8')  # Assume the first message from client is the unique ID
+    print(f"Client {client_id} connected from {addr[0]}")
 
-def broadcast(message,connection):
+    conn.send(b"Welcome to this chatroom!")  # Send bytes instead of a string
+
+    while True:
+        try:
+            message = conn.recv(2048)
+            if message:
+                decoded_message = message.decode('utf-8')
+                print(f"<{client_id}> {decoded_message}")
+                message_to_send = f"<{client_id}> {decoded_message}"
+                broadcast(message_to_send.encode('utf-8'), conn)  # Encode message before sending
+            else:
+                remove(conn)
+        except:
+            continue
+
+def broadcast(message, connection):
     for clients in list_of_clients:
-        if clients!=connection:
+        if clients != connection:
             try:
                 clients.send(message)
             except:
@@ -53,16 +48,11 @@ def remove(connection):
 
 while True:
     conn, addr = server.accept()
-    """
-    Accepts a connection request and stores two parameters, conn which is a socket object for that user, and addr which contains
-    the IP address of the client that just connected
-    """
     list_of_clients.append(conn)
-    print(addr[0] + " connected")
-    #maintains a list of clients for ease of broadcasting a message to all available people in the chatroom
-    #Prints the address of the person who just connected
-    start_new_thread(clientthread,(conn,addr))
-    #creates and individual thread for every user that connects
+    
+    # Start the client thread
+    thread = threading.Thread(target=clientthread, args=(conn, addr))
+    thread.start()
 
 conn.close()
 server.close()
--- client.py
+++ client.py
@@ -1,26 +1,49 @@
 import socket
-import select
 import sys
+import threading
+import uuid
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-if len(sys.argv) != 3:
-    print "Correct usage: script, IP address, port number"
-    exit()
-IP_address = str(sys.argv[1])
-Port = int(sys.argv[2])
+
+# Automatically fetch the local machine's IP (server IP)
+IP_address = socket.gethostbyname(socket.gethostname())  # Replace with server IP if needed
+Port = 8080  # Port number should match the server
+
 server.connect((IP_address, Port))
 
-while True:
-    sockets_list = [sys.stdin, server]
-    read_sockets,write_socket, error_socket = select.select(sockets_list, [], [])
-    for socks in read_sockets:
-        if socks == server:
-            message = socks.recv(2048)
-            print message
-        else:
-            message = sys.stdin.readline()
-            server.send(message)
-            sys.stdout.write("<You>")
-            sys.stdout.write(message)
-            sys.stdout.flush()
-server.close()
+# Generate a unique identifier for each client
+client_id = str(uuid.uuid4())[:8]  # Generate a random unique identifier (8 characters)
+
+# Send the client_id as the first message to the server
+server.send(client_id.encode('utf-8'))
+
+def receive_messages():
+    while True:
+        try:
+            message = server.recv(2048).decode('utf-8')
+            if message:
+                print(message)
+            else:
+                # Connection has been closed
+                server.close()
+                break
+        except:
+            # In case of any error, close the connection
+            server.close()
+            break
+
+def send_messages():
+    while True:
+        message = sys.stdin.readline()
+        message_to_send = f"[{client_id}] {message}"  # Prefix messages with client ID
+        server.send(message_to_send.encode('utf-8'))
+        sys.stdout.write(f"<You ({client_id})> ")
+        sys.stdout.write(message)
+        sys.stdout.flush()
+
+# Start the thread for receiving messages from the server
+receive_thread = threading.Thread(target=receive_messages)
+receive_thread.start()
+
+# Main thread for sending messages to the server
+send_messages()
